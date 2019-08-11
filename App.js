bd135d99e534f6b8c3887eeafbed0392ac372e7c
Adarsh Kumar <adarshkumar9820@gmail.com>
2019-08-11 20:52:21 +0800

    adding static image

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from "react-native";
 
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
+import mapsImage from "./src/assets/maps-icon.png";
 
 export default class App extends Component {
   state = {
@@ -14,7 +15,8 @@ export default class App extends Component {
       return {
         places: prevState.places.concat({
           key: Math.random(),
-          value: placeName
+          value: placeName,
+          image:mapsImage,
         })
       };
     });
